18704802d74e7c83673c38181dc7c726784494e9
adityakoolkarni <53965740+adityakoolkarni@users.noreply.github.com>
2020-01-28 00:42:51 -0800

    Working Code
    Needs parameter tuning

--- compute_batch_grad_descent.py
+++ compute_batch_grad_descent.py
@@ -35,18 +35,22 @@ def grad_descent(alpha, train_data, train_label, cv_data, cv_label, test_data, t
     shape_test_data = test_data.shape
     size_ftr_vctr = train_data.shape[1] 
     #wghts = np.zeros((1,size_ftr_vctr)) 
-    wghts = train_data[0,:].reshape((1,size_ftr_vctr)) / 10000
-    wghts = cv_data[0,:].reshape((1,size_ftr_vctr)) / 10000
+    wghts = train_data[0,:].reshape((1,size_ftr_vctr))
+    #wghts = cv_data[0,:].reshape((1,size_ftr_vctr)) / 10000
     print("Wrights",wghts)
     
     # Gradient Descent Algorithm
     
     good_wghts = wghts
     prev_loss = np.inf
+    print("CV_:ABEL",cv_label)
+    print("train_:ABEL",train_label)
     print("Shape of CV and Shape of Train", cv_data.shape,train_data.shape)
+    print("Number of red vs Non Red", sum(train_label),train_data.shape[0]-sum(train_label))
+    print("Number of red vs Non Red", sum(cv_label),cv_data.shape[0]-sum(cv_label))
     print("Train Data",train_data[0,0:20])
     print("CV Data",cv_data[0,0:20])
-    time.sleep(10)
+    print("test Data",test_data[0,0:20])
     for epoch in tqdm(range(M)):
         #Training 
         #print("Input to Logistic Regression",(train_data @ wghts.T).shape)
@@ -64,32 +68,44 @@ def grad_descent(alpha, train_data, train_label, cv_data, cv_label, test_data, t
 
         a = train_data @ wghts.T
         z = lgst_reg(a)
+        print("###################### Logistic of Train ######################## ",z)
         train_loss_arr = np.where(train_label == 1,np.log(lgst_reg(a)),np.log(1 - lgst_reg(a)))
+#        print("Train Loss",train_loss_arr)
+        #print("Train Loss ", train_loss)
 
         train_loss = -1*sum(train_loss_arr)
-        #print("train loss",train_loss)
+       # print("train loss",train_loss)
             
         train_loss_log[epoch] = train_loss / (shape_train_data[0] * 2) #Average over number of images and number of classes
         
         #CV loss calculation
         a = cv_data @ wghts.T
         z = lgst_reg(a)
+        #print("Summed Input to LGST for CV",a)
         cv_loss_arr = np.where(cv_label == 1,np.log(lgst_reg(a)),np.log(1 - lgst_reg(a)))
         cv_loss = -1 * sum(cv_loss_arr)
-
+        #print("CV Loss",cv_loss)
+#        print("CV Loss size and ", cv_loss_arr.shape)
         #print("cv loss",cv_loss)
-            
         cv_loss_log[epoch] = cv_loss / (shape_cv_data[0] * 2) #Average over number of images and number of classes
         if(cv_loss < prev_loss):
             #print("Weights getting better and the loss is ",cv_loss[0],prev_loss)
-            prev_loss = cv_loss[0]
+            prev_loss = cv_loss
             good_wghts = wghts
         else:
             pass
             #print("Loss is increasing",cv_loss[0])
         
     #################      Accuracy Calculation ##################
+
     
+    print("Summed Input to LGST for CV",a)
+    plt.plot(np.arange(M),train_loss_log,label='train')
+    plt.plot(np.arange(M),cv_loss_log,label='cv')
+    plt.legend()
+    plt.show()
+
+
     #Reshape the test data
     #shape_test_data = test_data.shape
     #resize_test_data = np.zeros((shape_test_data[0],size_ftr_vctr))
@@ -107,9 +123,9 @@ def grad_descent(alpha, train_data, train_label, cv_data, cv_label, test_data, t
     a = test_data @ good_wghts.T
     print("Summed Input to LGST",a)
     pred_img = np.where(lgst_reg(a) > 0.5, np.ones((shape_test_data[0],1)), np.zeros((shape_test_data[0],1)))
-    print("Summed Input to LGST",pred_img)
+    #print("Summed Input to LGST",pred_img)
     #print(pred_img)
-    est_img = pred_img.reshape(600,900)
+    est_img = pred_img.reshape(450,600)
     plt.imshow(est_img,cmap = 'gray')
     plt.show()
     #pred = sum(pred_img == test_label)
@@ -123,10 +139,7 @@ def grad_descent(alpha, train_data, train_label, cv_data, cv_label, test_data, t
     
     test_acc = pred / shape_test_data[0]
 
-    plt.plot(np.arange(M),train_loss_log,label='train')
-    plt.plot(np.arange(M),cv_loss_log,label='cv')
-    plt.legend()
-    plt.show()
+
     
     print("Classification Accuracy is ",test_acc)
     return np.array(cv_loss_log),np.array(train_loss_log), test_acc
